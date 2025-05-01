25be82bdeab423997ee82e9610680478ed9f4559
Palak-agarwal16 <161713902+Palak-agarwal16@users.noreply.github.com>
2025-05-02 00:36:39 +0530

    Add files via upload

---
+++ lab 6/Logger.java
@@ -0,0 +1,7 @@
+package lab_6;
+
+public final class Logger {
+	void logMessage(String message) {
+        System.out.println("Log: " + message);
+    }
+}
---
+++ lab 6/Main.java
@@ -0,0 +1,10 @@
+package lab_6;
+
+public class Main {
+	    public static void main(String[] args) {
+	        Circle circle = new Circle();
+	        circle.displayPI();
+	        circle.calculateArea(5);
+	    }
+	}
+

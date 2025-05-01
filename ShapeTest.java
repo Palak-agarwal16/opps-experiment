84c9cf79bbb2e8d3b17291a4bfe88e7c5ee5cc31
Palak-agarwal16 <161713902+Palak-agarwal16@users.noreply.github.com>
2025-05-02 00:27:37 +0530

    Delete ShapeTest.java

--- ShapeTest.java
+++
@@ -1,9 +0,0 @@
-public class ShapeTest {
-    public static void main(String[] args) {
-        Shape rect = new Rectangle();
-        Shape circ = new Circle();
-
-        rect.calculateArea();
-        circ.calculateArea();
-    }
-}

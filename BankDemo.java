195f70e1d9da694f63b5233e12fd7890a601459d
Palak-agarwal16 <161713902+Palak-agarwal16@users.noreply.github.com>
2025-05-01 23:54:21 +0530

    Delete BankDemo.java

--- BankDemo.java
+++
@@ -1,8 +0,0 @@
-public class BankDemo {
-    public static void main(String[] args) {
-        Account acc = new Account();
-        acc.deposit(1000);
-        acc.withdraw(300);
-        acc.displayBalance();
-    }
-}

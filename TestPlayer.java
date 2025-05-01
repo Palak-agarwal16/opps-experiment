a6e3e62ad1df8d16539ebb3c2531702596c02fba
Palak-agarwal16 <161713902+Palak-agarwal16@users.noreply.github.com>
2025-05-02 00:27:49 +0530

    Delete TestPlayer.java

--- TestPlayer.java
+++
@@ -1,9 +0,0 @@
-
-public class TestPlayer {
-    public static void main(String[] args) {
-        MusicPlayer player = new MusicPlayer();
-        player.play();
-        player.pause();
-        player.stop();
-    }
-}

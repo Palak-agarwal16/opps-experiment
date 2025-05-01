2fa5fba39c7c5a1d7575a9e743d6e06058144188
Palak-agarwal16 <161713902+Palak-agarwal16@users.noreply.github.com>
2025-05-02 00:18:31 +0530

    Delete PrivateMemberDemo.java

--- PrivateMemberDemo.java
+++
@@ -1,7 +0,0 @@
-
-public class PrivateMemberDemo {
-    public static void main(String[] args) {
-        SubClass obj = new SubClass();
-        obj.show(); // Ensure no invisible character here
-    }
-}

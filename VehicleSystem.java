9e444ada179ed227c82b023510a5247d08f494c8
Palak-agarwal16 <161713902+Palak-agarwal16@users.noreply.github.com>
2025-05-02 00:28:58 +0530

    Delete VehicleSystem.java

--- VehicleSystem.java
+++
@@ -1,8 +0,0 @@
-
-public class VehicleSystem {
-	 public static void main(String[] args) {
-	        ElectricCar tesla = new ElectricCar("Tesla", "Model S", 79999, 5, "Electric", 100, 2);
-	        tesla.displayDetails();
-	    }
-	
-}
